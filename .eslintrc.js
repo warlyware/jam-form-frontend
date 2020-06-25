e36c003aa8ac56c8a49da50cd3a374db36dd0a5a
Dan Ward <danward@gmail.com>
2020-06-24 17:40:22 -0700

    Add logic for adding new emails to mail list

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,9 @@ module.exports = {
   // add your custom rules here
   rules: {
     'nuxt/no-cjs-in-config': 'off',
-    'space-before-function-paren': 'off'
+    'space-before-function-paren': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/html-indent': 'off',
+    'vue/html-closing-bracket-newline': 'off'
   }
 }
--- components/PublishedForm.vue
+++ components/PublishedForm.vue
@@ -6,21 +6,45 @@
     <p class="mb-2">
       {{form.text}}
     </p>
-    <input class="p-2 border border-black" type="text">
+    <input v-model="email"
+    class="p-2 border border-black"
+    type="text">
     <br>
-    <button class="bg-blue-600 p-2 rounded mt-4 text-white">
+    <button class="bg-blue-600 p-2 rounded mt-4 text-white"
+    @click="addEmail">
       {{ form.buttonText || 'Subscribe' }}
     </button>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
+import addEmail from '~/apollo/mutations/email-list/add-email'
 export default {
   props: {
     form: {
       type: Object,
       requred: true,
       default: () => ({})
+    }
+  },
+  data() {
+    return {
+      email: ''
+    }
+  },
+  computed: mapState(['currentUser']),
+  methods: {
+    addEmail() {
+      this.$apollo.mutate({
+        mutation: addEmail,
+        variables: {
+          email: this.email,
+          campaignId: this.form.campaignId,
+          userId: this.currentUser.id
+        }
+      })
     }
   }
 }
